a7f3a55219d70a36333ac0f4ee32db53ae11ea14
Hyunsung <jodog0412@gmail.com>
2024-02-13 11:05:41 +0900

    [refactor] make code easier

--- read_db.py
+++ read_db.py
@@ -8,16 +8,16 @@ from gridfs import GridFS
 # mongo_db.fs.files -> _id, filename, chunkSize, length, uploadDate
 
 fs = GridFS(mongo_db)
-chunk_docs, meta_docs = mongo_db["fs.chunks"], mongo_db["fs.files"]
-chunks, metas = [chunk for chunk in chunk_docs.find()], [doc for doc in meta_docs.find()]
-last_meta_doc=metas[-1]
+chunk_table, meta_table = mongo_db["fs.chunks"], mongo_db["fs.files"]
+chunks, metas = [chunk for chunk in chunk_table.find()], [doc for doc in meta_table.find()]
+last_meta=metas[-1]
 
 # find(read) generated file by name 
 name='xxx_0.jpg'
 # 1. name으로부터 file_id 찾기
-file_id = meta_docs.find_one({"filename": name})['_id']
+file_id = meta_table.find_one({"filename": name})['_id']
 # 2. file_id로부터 chunks 찾기
-chunks=chunk_docs.find({"files_id":file_id})
+chunks=chunk_table.find({"files_id":file_id})
 # 3. 이미지 출력
 img = b''.join(chunk['data'] for chunk in chunks)
 img_output = Image.open(BytesIO(img))
