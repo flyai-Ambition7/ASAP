5f868ff4e9b7a82b79c1d4b042f6c8e7a097a6dc
Hyunsung <jodog0412@gmail.com>
2024-02-13 11:01:53 +0900

    [refactor] simplify code for read_db

--- read_db.py
+++ read_db.py
@@ -1,26 +1,24 @@
 from PIL import Image
 from io import BytesIO
-from pymongo import MongoClient
 from db import client, mongo_db
 from gridfs import GridFS
+
+# mongo_db : mongodb 객체
+# mongo_db.collections -> fs.files, fs.chunks, user
+# mongo_db.fs.files -> _id, filename, chunkSize, length, uploadDate
+
 fs = GridFS(mongo_db)
-print(mongo_db.list_collection_names())
 chunk_docs, meta_docs = mongo_db["fs.chunks"], mongo_db["fs.files"]
-metas=[doc for doc in meta_docs.find()]
-file=metas[-1]
-print(file)
+chunks, metas = [chunk for chunk in chunk_docs.find()], [doc for doc in meta_docs.find()]
+last_meta_doc=metas[-1]
 
-# chunks=[doc for doc in chunk_docs.find()]
-# print(chunks)
-# for doc in docs:
-    # print(doc)
-
-# name='yaong_0.jpg'
-# file_id = meta_docs.find_one({"filename": name})['_id']
-# chunks=chunk_docs.find({"files_id":file_id})
-# img=b''
-# for chunk in chunks:
-#     img+=chunk['data']
-# print(img)
-# result = Image.open(BytesIO(img))
-# result.show()  # 이미지를 보여줍니다.
+# find(read) generated file by name 
+name='xxx_0.jpg'
+# 1. name으로부터 file_id 찾기
+file_id = meta_docs.find_one({"filename": name})['_id']
+# 2. file_id로부터 chunks 찾기
+chunks=chunk_docs.find({"files_id":file_id})
+# 3. 이미지 출력
+img = b''.join(chunk['data'] for chunk in chunks)
+img_output = Image.open(BytesIO(img))
+img_output.show()  # 이미지를 보여줍니다.
