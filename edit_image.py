dd10fce82f7ec093379fddc42092da6fdbac61d1
Hyunsung Lee <jodog0412@gmail.com>
2024-02-13 05:12:23 +0000

    [feat] DB update by result image using opencv

--- edit_image.py
+++ edit_image.py
@@ -5,5 +5,6 @@ import numpy as np
 
 def add_images(img1,img2):
     img1,img2=tuple(map(lambda img:np.array(img),[img1,img2]))
-    img_output = Image.fromarray(cv2.add(img1, img2))
-    return img_output
+    img_result=cv2.add(img1,img2)
+    img_bytes = cv2.imencode('.jpg', img_result)[1].tobytes()
+    return img_bytes
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from PIL import Image
 from fastapi import FastAPI
 from draw_image import *
+from edit_image import *
 from eval_image import evalulate_image
 from update_db import *
 import cv2
@@ -15,15 +16,10 @@ async def root():
 @app.get("/draw")
 def draw(user_id:str,prompt:str):
     start=time.time()
-    DALLE_img,DALLE_score=draw_filtered_image_by_DALLE(prompt)
+    DALLE_img,DALLE_acc=draw_filtered_image_by_DALLE(prompt)
     SD_img=draw_image_by_SD(prompt)
-    # DALLE_img.save(f'{BASE_PATH}/DALLE_{int(start)}.jpg')
-    # SD_img.save(f'{BASE_PATH}/SD_{int(start)}.jpg')
-    img_output=cv2.add(DALLE_img,SD_img)
-    img_output.save(f'{BASE_PATH}/RESULT_{int(start)}')
-    # update_image(DALLE_img,user_id,start)
+    img_output=add_images(DALLE_img,SD_img)
+    update_image(img_output,user_id,int(start))
     end=time.time()
-    return {
-            "output_img_info":img_output.size(),
-            "DALLE_score":f"{DALLE_score:.2f}",
+    return {"DALLE_accuracy":f"{DALLE_acc:.2f}",
             "time_consumption":f"{end-start:.2f}"}
