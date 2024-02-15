8cc3b9037289453664ea97dbbc2421e2dbaec73f
Hyunsung Lee <jodog0412@gmail.com>
2024-02-15 06:53:30 +0000

    [feat] upgrade images addition module

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from edit_image import add_images
 from eval_image import evalulate_image
 from db.update_db import update_text_to_db, update_image_to_db
 from db.read_db import *
+import numpy as np
 
 app=FastAPI()
 BASE_PATH="samples"
@@ -26,30 +27,42 @@ async def upload(user_id:str,text_prompt:str,bg_prompt:str,img_input:UploadFile)
         "time_consumption":f"{end-start:.2f}"
     }
 
-@app.get("/read")
-async def read():
+@app.get("/read_text")
+async def read_text():
     start = time.time()
-    img_input, img_file_name, text_prompt, bg_prompt = read_infos_from_db(img_chunk_tbl,img_meta_tbl,text_tbl)
+    _, _, text_prompt, bg_prompt = read_infos_from_db(img_chunk_tbl,img_meta_tbl,text_tbl)
     end=time.time()
     return {
-        "img_file_name":img_file_name,
         "text_prompt":text_prompt,
         "bg_prompt":bg_prompt,
         "time_consumption":f"{end-start:.2f}"
     }
 
-@app.get("/draw")
+@app.get("/read_img")
+async def read_img():
+    start = time.time()
+    img_output=get_img_from_db(img_chunk_tbl,img_meta_tbl)
+    img_output.show()
+    img_output.close()
+    end=time.time()
+    return {
+        "img_size":img_output.size,
+        "time_consumption":f"{end-start:.2f}"
+    }
+
+@app.post("/draw")
 async def draw():
     start=time.time()
     img_input, img_file_name, text_prompt, image_prompt = read_infos_from_db(img_chunk_tbl,img_meta_tbl,text_tbl)
     user_id = img_file_name.split('_')[0]
     DALLE_img, DALLE_acc = draw_filtered_image_by_DALLE(text_prompt)
     SD_img = draw_image_by_SD(img_input,image_prompt)
-    img_output = add_images(DALLE_img,SD_img)
-    update_image_to_db(img_output,
-                       user_id,
-                       upload_time=int(start),
-                       isinput=False)
+    SD_img.show()
+    # img_output = add_images(DALLE_img,SD_img)
+    # update_image_to_db(img_output,
+    #                    user_id,
+    #                    upload_time=int(start),
+    #                    isinput=False)
     end=time.time()
     return {
         "DALLE_accuracy":f"{DALLE_acc:.2f}",
